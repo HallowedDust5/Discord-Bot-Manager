4e7a1066865d2ef8c83e845b4d51c9281454fcfc
HallowedDust5 <77157773+HallowedDust5@users.noreply.github.com>
2021-06-21 15:02:58 -0400

    Replaced HTML with eJS

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-//Imporrts
 const express = require('express');
 const fs = require('fs');
-const ifuncs = require("./indexfuncs");
 
-//Initializes the server and the port and 
 const app = express();
-const port = 3000;
+var bots = fs.readFileSync('./logs/bots.json')
+
+app.set('view engine','ejs');
+app.set('views','views');
 
 
-//Sends this folder to the client as their clientside application
-app.use(express.static('public'));
-app.use(express.urlencoded());
-app.use(express.json());
-
- 
-//Starts the server and when it starts listening to the port, it writes when it started listening
-app.listen(port, ()=>{
-    // fs.appendFile("logs/server-uptime.txt", `Server started at ${Date.now()}\n`, (err) => {
-    //     if(err) {
-    //         return console.log(err);
-    //     }
-    //     console.log("Time saved to server-uptime");
-    // }); 
-});
-
-app.post('/', (req, res)=>{
-
-    console.log(3);
+app.listen(3000);
 
 
 
-    res.end();
+app.get('/',(req,res)=>{
+    res.render('index', {bots});
 });
--- public/funcs.js
+++
@@ -1,8 +0,0 @@
-function ShowHideDiv() {
-    if(document.getElementById("add").checked){
-        document.getElementById("addTextBox").style.visibility = 'visible';
-    }
-    else{
-        document.getElementById("addTextBox").style.visibility = 'hidden';
-    }
-}
