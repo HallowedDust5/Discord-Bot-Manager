6e35dbb6d974e9df5916687072e621ae6be4e998
HallowedDust5 <77157773+HallowedDust5@users.noreply.github.com>
2021-06-26 20:38:23 -0400

    Retry Page partially added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
+const funcs = require('./indexfuncs')
 
 //Constants
 const app = express();
-const passHash = bcrypt.hashSync(fs.readFileSync('./logs/passphrase.txt').toString());
-
+const passHash = bcrypt.hashSync(fs.readFileSync('./logs/passphrase.txt').toString());//Hashed version of password
+let bots = JSON.parse(fs.readFileSync('./logs/bots.json'));
 
 
 //View Engine
@@ -14,13 +15,13 @@ app.set('views','views');
 
 //Middleware
 app.use(express.urlencoded({extended:true}));
+//app.use(()=>{bots = JSON.parse(fs.readFileSync('./logs/bots.json'));});
 
 app.listen(3000);
 
 
 
 app.get('/',(req,res)=>{
-    let bots = JSON.parse(fs.readFileSync('./logs/bots.json'));
     res.render('index', {bots});
 });
 
@@ -28,12 +29,25 @@ app.post('/',(req,res)=>{
     let formData = req.body;
     console.log(formData);
     if(bcrypt.compareSync(formData.pwd,passHash)){
-        
+        switch(formData.action){
+            case 'run':
+                funcs.runBot();
+                break;
+            case 'update':
+                funcs.updateBot();
+                break;
+            case 'add':
+                funcs.addBot();
+                break;
+            default:
+                
+
+        }
     }
     else{
 
     }
-
+    res.render('retry');
     res.redirect('/');
 });
 
--- indexfuncs.js
+++ indexfuncs.js
@@ -3,14 +3,19 @@
 
 
 function runBot(){
-
+    console.log('run');
 }
 
 
 function updateBot(){
+    console.log('update');
 
 }
 
 function addBot(){
-    
+    console.log('add');
+
     }
+
+
+module.exports = {runBot,updateBot,addBot};
