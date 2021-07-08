3ac1922b3738f5d818b515ea7908bc4482c7e6ed
HallowedDust5 <charizardexlvl100@gmail.com>
2021-07-07 20:37:50 -0400

    Fixed bug where theBotChoice was undefined

--- indexfuncs.js
+++ indexfuncs.js
@@ -17,8 +17,12 @@ function runBot(formData){
 
 function updateBot(formData){
     let bots = JSON.parse(fs.readFileSync('./logs/bots.json'));
-    let theBotChoice = bots.find(bot=>{bot.name===formData.botChoice;});
-    if(theBotChoice===undefined){return 404;}
+    let theBotChoice;
+    bots.forEach(bot => {
+        if (bot.name===formData.botChoice) {
+            theBotChoice=bot;
+        }
+    });    if(theBotChoice===undefined){return 404;}
     exec(`cd discord_bots\\${formData.botChoice} & git pull ${theBotChoice.link}`,
     (err,stdout,stderr)=>{
         if(err){
@@ -38,30 +42,36 @@ function addBot(formData){
             console.log(err);
             return 404;
         }
-        console.log(stdout);
+        let bots = JSON.parse(fs.readFileSync('./logs/bots.json'));
+        bots.push({name:formData.addOptions[0],link:formData.addOptions[1]});
+        fs.writeFile('./logs/bots.json',JSON.stringify(bots),(err)=>{
+            if(err){return 404;}
+        });
     }); 
 }
 
 function delBot(formData){
     let bots = JSON.parse(fs.readFileSync('./logs/bots.json'));
-    let theBotChoice = bots.find(bot=>{bot.name===formData.botChoice;});
+    let theBotChoice;
+    bots.forEach(bot => {
+        if (bot.name===formData.botChoice) {
+            theBotChoice=bot;
+        }
+    });
     if(theBotChoice===undefined){return 404;}
-    console.log(theBotChoice);
-    fs.rmdir(`discord_bots/${theBotChoice}`,{recursive:true} ,(err)=>{
+    fs.rmdir(`./discord_bots/${theBotChoice.name}`,{recursive:true} ,(err)=>{
+        console.log('error');
         if(err){
             //add to logs and return a retry
-            console.log(err);
             return 404;
         }
         //Take out the bot whose index is where the bot name is equal to the form data bot choice
-        bots.splice(bots.indexOf(bots.find(bot => {bot.name===theBotChoice;})));
-        console.log(bots);
+        bots.splice(bots.findIndex(bot=>{bot.name===formData.botChoice;}));
         fs.writeFile('./logs/bots.json',JSON.stringify(bots),(err)=>{
             if (err){
                 //Make it add to logs
                 return 404;
             }
-            console.log('spliced');
 
         });
         
