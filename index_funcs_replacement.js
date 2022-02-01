b3ffc3d7c128e97c2696e8e114c086648b500e35
HallowedDust5 <charizardexlvl100@gmail.com>
2022-02-01 13:35:04 -0500

    Added helper functions and got rid of name input

--- index_funcs_replacement.js
+++ index_funcs_replacement.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 
-
+const ssh_link_re = /(?<=\/).*(?=\.git)/;
+const https_link_re = /(?<=\/\/github.com\/.*\/).*(?=\.git)/;
 /*
 Commands
     Redirect into bot folder
@@ -32,26 +33,49 @@ function updateBot(formData) {
 
 /*
 Commands
-    Redirect into discord bots
-    mkdir with bot name
-    Redirect into bot folder
-    git pull
-    npm install to upgrade from package.json
+    Make sure it's a new bot
+    Store list of all bots right now
+    git clone into discord_bots
+    Add repo as origin
+    Figure out difference in what was added and add that folder name to bots.json
+        This option is better because the other one runs something directly from the user
+
 
 Changed Data
     Add given bots to bots.json
 */
 function addBot(formData) {
-    
-    fs.mkdir(`./discord_bots/${formData.addOptions[0]}`,(err)=>{
-        if (err) {
-            return {isError:true,msg:'Problem making directory'};
-        }
-    });
+    let repo_name = getRepoName(formData.addOptions[0]);
+
+    if(!isNewBot(formData.addOptions[0])){
+        return {isError:true,msg:`${repo_name} is already present`};
+    }
+
 
 }
 
 
+
+
+//addBot helper functions
+const getRepoName = (repo_link)=>{
+    if(repo_link.contains('git@github.com')){
+        return repo_link.match(ssh_link_re)[0];
+    }
+    else if(repo_link.contains(`https://github.com`)){
+        return repo_link.match(https_link_re)[0];
+    }
+    return null;
+};
+
+
+const isNewBot = (repo_link) => {
+    repo_name = getRepoName(repo_link);
+    fs.readdir('./discord_bots',(err,files)=>{
+        return files.includes(repo_name);
+    });
+};
+//End of addBot helper functions
 
 
 
@@ -77,7 +101,13 @@ function cleanString(given_string) {
     
 }
 
-
+//Server log function
+function serverLog(line){
+    line = '\nOperation Message:'+line+` at ${new Date()}`;
+    fs.appendFile('logs/server-uptime.txt',line,(err)=>{
+        if(err){console.log(`Logging failed at ${new Date()}`);}
+    });
+}
 
 
 
